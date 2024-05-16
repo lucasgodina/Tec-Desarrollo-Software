1311c2d67f1485dff81d7c63a0c5c885babc1d82
Lucas Godina <lucasgodina@gmail.com>
2024-05-16 20:12:28 -0300

    Web - Finalizado preparcial y retoques finales del menu para el parcial

--- Web/menu/script.js
+++ Web/menu/script.js
@@ -1,21 +1,24 @@
-const hamburger = document.querySelector(".hamburger");
-const navLink = document.querySelector(".nav__link");
+const hamburger = document.querySelector('.hamburger');
+const navLink = document.querySelector('.nav__link');
 
-hamburger.addEventListener("click", () => {
-  navLink.classList.toggle("hide");
+hamburger.addEventListener('click', () => {
+	navLink.classList.toggle('hide');
 });
 
 /*
 Call a function when a button is clicked:
- <button onclick="myFunction()">Click me</button>
+    <button onclick="myFunction()">Click me</button>
 
- Call a function when a user changes the 
- selected option of a <select> element:
-  <select onchange="myFunction()"> 
+Call a function when a user changes the 
+selected option of a <select> element:
+    <select onchange="myFunction()"> 
 
-  Call a function when a form is submitted:
+Call a function when a form is submitted:
     <form onsubmit="myFunction()">
         Enter name: <input type="text">
         <input type="submit">
     </form> 
+
+Call a function when an input field gets focus:
+    <input type="text" onfocus="myFunction()"> 
 */
