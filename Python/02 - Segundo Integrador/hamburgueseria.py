9510a13cb052ea0c7dc3628bba8aa392319e9028
Lucas Godina <lucasgodina@gmail.com>
2024-10-02 15:06:18 -0300

    Refactor menu function and add pedido function
    - Refactored the menu function in hamburgueseria.py to improve readability and maintainability.
    - Added a new function called pedido to handle the confirmation and processing of customer orders.
    - Updated the main loop in ejercicio1.py to handle pygame events and display a colored window.

--- Python/02 - Segundo Integrador/hamburgueseria.py
+++ Python/02 - Segundo Integrador/hamburgueseria.py
@@ -1,5 +1,9 @@
+import datetime
+
+
 def menu():
-    while True:
+    running = True
+    while running:
         print("Bienvenido a la Hamburguesas IT")
         encargado = input("Ingrese el nombre del encargad@: ")
 
@@ -11,24 +15,51 @@ def menu():
         print("2. Cambio de encargado")
         print("3. Salir")
 
-        choice = input("Seleccione una opción: ")
+        opcion = input("Seleccione una opción: ")
 
-        if choice == "1":
+        if opcion == "1":
             cliente = input("Ingrese el nombre del cliente: ")
             cant_combo_s = int(input("Ingrese la cantidad de combos simples: "))
             cant_combo_d = int(input("Ingrese la cantidad de combos dobles: "))
             cant_combo_t = int(input("Ingrese la cantidad de combos triples: "))
             cant_flurby = int(input("Ingrese la cantidad de Flurbys: "))
+            total = (
+                cant_combo_s * 5 + cant_combo_d * 6 + cant_combo_t * 7 + cant_flurby * 2
+            )
+            print(f"Total a pagar: ${total}")
+            abona = input(int(f"El cliente abona con: $"))
+            vuelto = abona - total
 
-        elif choice == "2":
-            print("Haciendo un pedido...")
-            # Aquí puedes agregar el código para hacer un pedido
-        elif choice == "3":
+            print(f"Pedido para {cliente}")
+            print(f"Total a pagar: ${total}")
+            print(f"Abona con: ${abona}")
+            print(f"Vuelto: ${vuelto}")
+            confirma = input("¿Desea confirmar el pedido? (S/N): ")
+
+            while confirma.lower != "s" and confirma.lower != "n":
+                print("Opción no válida. Por favor, intente de nuevo.")
+                confirma = input("¿Desea confirmar el pedido? (S/N): ")
+
+            if confirma.lower == "s":
+                pedido()
+            elif confirma.lower == "n":
+                print("Pedido cancelado")
+
+        elif opcion == "2":
+            print("Cambiando encargado..")
+            encargado = input("Ingrese el nombre del nuevo encargad@: ")
+
+        elif opcion == "3":
             print("Saliendo...")
-            # Aquí puedes agregar el código para salir del programa
+            running = False
         else:
             print("Opción no válida. Por favor, intente de nuevo.")
 
 
-# Llamar a la función menu
-menu()
+def pedido(cliente, cant_combo_s, cant_combo_d, cant_combo_t, cant_flurby, total):
+    print(f"Pedido")
+
+
+# Llamado a la función menu
+if __name__ == "__main__":
+    menu()
